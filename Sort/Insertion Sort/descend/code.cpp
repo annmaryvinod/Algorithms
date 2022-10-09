069b01e8da4caf6859b240afc289d7cf93f63d16
annmaryvinod <annmarypriyavinod@gmail.com>
2022-10-09 20:14:26 +0530

    insertion sort descending order

---
+++ Sort/Insertion Sort/descend/code.cpp
@@ -0,0 +1,54 @@
+// Insertion sort for descending order : only change in key>a[j]
+
+#include <iostream>
+using namespace std;
+
+void readArray(int a[],int n){
+    for(int i =0;i<n;i++){
+        cin>>a[i];
+    }
+}
+
+void printArray(int a[],int n){
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<"\t";
+    }
+    cout<<endl;
+}
+
+void insertionSort(int a[],int n){
+    int i,j,key;
+
+    for(i=1;i<n;i++){
+        key = a[i];
+        j =i-1;
+
+        while(j>=0 && key>a[j]){
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=key;
+    }
+}
+
+
+
+int main(){
+    int i,n;
+    cout<<"Enter the array size :\n";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the array elements:\n";
+    readArray(arr,n);
+
+    cout<<"Entered array: \n";
+    printArray(arr,n);
+
+    cout<<"Sorted array:\n";
+    insertionSort(arr,n);
+    printArray(arr,n);
+
+    return 0;
+    
+}
